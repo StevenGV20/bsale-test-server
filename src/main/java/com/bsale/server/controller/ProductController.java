678ab9e08958c991b0d5bde85d44e515e5427e8e
StevenGV20 <stevenguadana@gmail.com>
2022-07-10 00:50:39 -0500

    controlando errores en las apis

--- src/main/java/com/bsale/server/controller/ProductController.java
+++ src/main/java/com/bsale/server/controller/ProductController.java
@@ -1,12 +1,17 @@
 package com.bsale.server.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,27 +41,53 @@ public class ProductController {
 	@GetMapping("/listAll")
 	@ApiOperation("Obtener todos los productos")
 	@ApiResponse(code = 200, message = "OK")
-	public Page<Product> listProduct(@ApiParam(value = "El numero de pagina a filtrar", required = true, example = "1")Integer page){
-		Pageable pageable = PageRequest.of(page, 8);
+	public ResponseEntity<Object> listProduct(@ApiParam(value = "El numero de pagina a filtrar", required = true, example = "1")Integer page){
 		
-		return product.listAllProducts(pageable);
+		try {
+			if(page == null || page < 0) {
+				Map<String, Object> response = new HashMap<String, Object>();
+				response.put("error", "Ingrese una categoria o numero de pagina mayor o igual a 0.");
+				return new ResponseEntity<Object>(response,new HttpHeaders(),HttpStatus.BAD_REQUEST);
+			}
+			else {
+				Pageable pageable = PageRequest.of(page, 8);
+				return new ResponseEntity<Object>(product.listAllProducts(pageable),new HttpHeaders(),HttpStatus.ACCEPTED);
+			}
+		} catch (Exception e) {
+			Map<String, Object> response = new HashMap<String, Object>();
+			response.put("error", "Hubo un error en el proceso, verifica bien tus datos");
+			return new ResponseEntity<Object>(response,new HttpHeaders(),HttpStatus.BAD_REQUEST);
+		}
 	}
 	
 	@GetMapping("/listByCategory")
-	@ApiOperation("Obtener todos los productos")
+	@ApiOperation("Obtener todos los productos segun la categoria")
 	@ApiResponse(code = 200, message = "OK")
-	public Page<Product> listByCategory(@ApiParam(value = "El id de la categoria a filtrar", required = true, example = "1") Long cat, 
-			@ApiParam(value = "El numero de pagina a filtrar", required = true, example = "1")Integer page){
-		Pageable pageable = PageRequest.of(page, 8);
-		List<Long> listCat = new ArrayList<Long>();
-		if(cat > 0) {
-			listCat.add(cat);
-		}else {
-			category.listCategory().forEach(c -> {
-				listCat.add(c.getIdcategory());
-			});
+	public ResponseEntity<Object> listByCategory(@ApiParam(value = "El id de la categoria a filtrar", required = true, example = "1") Long cat, 
+			@ApiParam(value = "El numero de pagina a filtrar", required = true, example = "1", defaultValue = "0")Integer page){
+		try {
+			if(cat<0 || page < 0) {
+				Map<String, Object> response = new HashMap<String, Object>();
+				response.put("error", "Ingrese numero de pagina mayor o igual a 0.");
+				return new ResponseEntity<Object>(response,new HttpHeaders(),HttpStatus.BAD_REQUEST);
+			}
+			else {
+				Pageable pageable = PageRequest.of(page, 8);
+				List<Long> listCat = new ArrayList<Long>();
+				if(cat > 0) {
+					listCat.add(cat);
+				}else {
+					category.listCategory().forEach(c -> {
+						listCat.add(c.getIdcategory());
+					});
+				}
+				return new ResponseEntity<Object>(product.listProductsByCategory(listCat, pageable),new HttpHeaders(),HttpStatus.ACCEPTED);
+			}
+		} catch (Exception e) {
+			Map<String, Object> response = new HashMap<String, Object>();
+			response.put("error", "Hubo un error en el proceso, verifica bien tus datos");
+			return new ResponseEntity<Object>(response,new HttpHeaders(),HttpStatus.BAD_REQUEST);
 		}
-		return product.listProductsByCategory(listCat, pageable);
 	}
 	
 	@GetMapping("/listByName")
@@ -65,44 +96,62 @@ public class ProductController {
 		@ApiResponse(code = 200, message = "OK"),
 		@ApiResponse(code = 404, message = "Producto no encontrado")
 	})
-	public List<Product> listByName(@ApiParam(value = "El nombre del producto", required = true, example = "Energetica")String name){
-		return product.listProductsByName(name);
+	public ResponseEntity<Object> listByName(@ApiParam(value = "El nombre del producto", required = true, example = "Energetica")String name){
+		if(name == null || name.isEmpty()) {
+			Map<String, Object> response = new HashMap<String, Object>();
+			response.put("error", "No has ingresado ningun nombre");
+			return new ResponseEntity<Object>(response,new HttpHeaders(),HttpStatus.BAD_REQUEST);
+		}else {
+			return new ResponseEntity<Object>(product.listProductsByName(name),new HttpHeaders(),HttpStatus.BAD_REQUEST);
+		}
 	}
 	
 	@GetMapping("/filter")
-	@ApiOperation("Obtener todos los productos")
+	@ApiOperation("Obtener todos los productos segun el filtro")
 	@ApiResponse(code = 200, message = "OK")
-	public Page<Product> listProductByFilter(
+	public ResponseEntity<Object> listProductByFilter(
 			@ApiParam(value = "El tipo de filtro", required = true, example = "NameASC",defaultValue = "All",allowableValues =  "All, NameASC, NameDESC, PriceASC, PriceDESC") String type, 
-			@ApiParam(value = "El id de la categoria a filtrar", required = true, example = "1") Long cat, 
-			@ApiParam(value = "El numero de pagina a filtrar", required = true, example = "1")Integer page){
-		Pageable pageable = PageRequest.of(page, 8);
-		Page<Product> lista = null;
-		List<Long> listCat = new ArrayList<Long>();
-		if(cat > 0) {
-			listCat.add(cat);
-		}else {
-			category.listCategory().forEach(c -> {
-				listCat.add(c.getIdcategory());
-			});
+			@ApiParam(value = "El id de la categoria a filtrar", required = true, example = "1", defaultValue = "0") Long cat, 
+			@ApiParam(value = "El numero de pagina a filtrar", required = true, example = "1", defaultValue = "0")Integer page){
+		try {
+			if(cat<0 || page < 0) {
+				Map<String, Object> response = new HashMap<String, Object>();
+				response.put("error", "Hubo un error en el proceso verifica bien tus datos");
+				return new ResponseEntity<Object>(response,new HttpHeaders(),HttpStatus.BAD_REQUEST);
+			}else {
+				Pageable pageable = PageRequest.of(page, 8);
+				Page<Product> lista = null;
+				List<Long> listCat = new ArrayList<Long>();
+				if(cat > 0) {
+					listCat.add(cat);
+				}else {
+					category.listCategory().forEach(c -> {
+						listCat.add(c.getIdcategory());
+					});
+				}
+				switch (type) {
+					case "NameASC":
+						lista = product.listProductByNameAZ(listCat,pageable);
+						break;
+					case "NameDESC":
+						lista = product.listProductByNameZA(listCat,pageable);
+						break;
+					case "PriceASC":
+						lista = product.listProductByPriceAsc(listCat,pageable);
+						break;
+					case "PriceDESC":
+						lista = product.listProductByPriceDesc(listCat,pageable);
+						break;
+					default:
+						lista = product.listProductsByCategory(listCat,pageable);
+				}
+				
+				return new ResponseEntity<Object>(lista,new HttpHeaders(),HttpStatus.ACCEPTED);
+			}
+		} catch (Exception e) {
+			Map<String, Object> response = new HashMap<String, Object>();
+			response.put("error", "Hubo un error en el proceso, verifica bien tus datos");
+			return new ResponseEntity<Object>(response,new HttpHeaders(),HttpStatus.BAD_REQUEST);
 		}
-		switch (type) {
-			case "NameASC":
-				lista = product.listProductByNameAZ(listCat,pageable);
-				break;
-			case "NameDESC":
-				lista = product.listProductByNameZA(listCat,pageable);
-				break;
-			case "PriceASC":
-				lista = product.listProductByPriceAsc(listCat,pageable);
-				break;
-			case "PriceDESC":
-				lista = product.listProductByPriceDesc(listCat,pageable);
-				break;
-			default:
-				lista = product.listProductsByCategory(listCat,pageable);
-		}
-		
-		return lista;
 	}
 }
